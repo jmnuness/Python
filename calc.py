3e95efe48be4eb0bdb847ca0c92bdead52b29d38
jmnuness <61925436+jmnuness@users.noreply.github.com>
2020-05-21 18:54:53 -0300

    Calculadora.py
    Primeira calculadora com python

---
+++ calc.py
@@ -0,0 +1,77 @@
+# Calculadora Simples by João Marcos Nunes
+print ("Calculadora Simples by João Marcos Nunes\n\t")
+
+# Variáveis 
+numero1 = 0
+numero2 = 0
+resultado = 0
+operacao = ''
+
+# Entradas dos numeros a serem usados
+numero1 = int(input('Digite o primeiro numero: \t'))
+numero2 = int(input('Digite o segundo numero: \t'))
+
+# Antes da entrada da operação, mostrar a lista de operações
+print ('Lista de operações: +, -, /, x')
+operacao = input('Digite a operação desejada: \n\t')
+
+# Condições para que seja realizada cada operação
+if operacao == ('+'):
+  resultado = numero1 + numero2
+  print ("Este é o resultado: ", (resultado))
+elif operacao == ('-'):
+  resultado = numero1 + numero2
+  print ("Este é o resultado: ", (resultado))
+elif operacao == ('/'):
+  resultado = numero1 / numero2
+  print ("Este é o resultado: ", (resultado))
+elif operacao ==('x'):
+  resultado = numero1*numero2
+  print ("Este é o resultado: ", (resultado))
+#Caso o usuario não coloque um valor válido
+else:
+  print("Operação inválida, tente novamente!")
+
+
+# Calculadora Simples by João Marcos Nunes
+print ("Calculadora Simples by João Marcos Nunes\n\t")
+
+# Variáveis 
+numero1 = 0
+numero2 = 0
+resultado = 0
+operacao = ''
+
+# Entradas dos numeros a serem usados
+numero1 = float(input('Digite o primeiro numero: \t'))
+numero2 = float(input('Digite o segundo numero: \t'))
+
+# Antes da entrada da operação, mostrar a lista de operações
+print ('Lista de operações: +, -, /, x')
+operacao = input('Digite a operação desejada: \n\t')
+
+# Condições para que seja realizada cada operação
+if operacao == ('+'):
+  resultado = numero1 + numero2
+  print ("Este é o resultado: ", (resultado))
+elif operacao == ('-'):
+  resultado = numero1 + numero2
+  print ("Este é o resultado: ", (resultado))
+elif operacao == ('/'):
+  resultado = numero1 / numero2
+  print ("Este é o resultado: ", (resultado))
+elif operacao ==('x'):
+  resultado = numero1*numero2
+  print ("Este é o resultado: ", (resultado))
+#Caso o usuario não coloque um valor válido
+else:
+  print("Operação inválida, tente novamente!")
+
+
+
+
+
+
+
+
+
